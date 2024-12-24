268fa8841299d43ea90c40fe6ff20f3f679946bf
Igor <asachiyigor@gmail.com>
2024-12-24 23:04:30 +0300

    Задача "feature-BJS2-41933-Igor" BJS2-41933
    add corrections

--- src/main/java/faang/school/payment_service/service/payment/strategy/InitiatePaymentStrategy.java
+++ src/main/java/faang/school/payment_service/service/payment/strategy/InitiatePaymentStrategy.java
@@ -8,8 +8,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
-import java.time.LocalDateTime;
-
 @Slf4j
 @Component
 @RequiredArgsConstructor
